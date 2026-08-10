ecbf7dd8f89d6be7b2d69a7a9b6bb1aeb3781c85
Smokister <hardikjoshi246@gmail.com>
2026-08-10 15:29:56 +0530

    Add files via upload

---
+++ d1q1.c
@@ -0,0 +1,29 @@
+/*
+Q1: Write a program to input two numbers and display their sum.
+
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+float a, b, sum;
+printf("enter first num:\n");
+scanf("%f", & a);
+printf("enter second num:\n");
+scanf("%f", & b);
+sum = a + b;
+printf("sum of the two numbers is: %0.2f\n", sum);
+return 0;
+}
