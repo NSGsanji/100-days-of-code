f0632bde17eff8289f43b1a0adf99363089009b0
Smokister <hardikjoshi246@gmail.com>
2026-09-02 12:12:25 +0530

    Create q44.c

---
+++ day22/q44.c
@@ -0,0 +1,39 @@
+//Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 3.3
+
+Input 2:
+5
+Output 2:
+Approximate sum: 4.4
+
+*/
+#include<stdio.h> 
+int main() 
+{ 
+    int n,i;
+    float s=0; 
+    
+    printf("Enter a number:"); 
+    scanf("%d",&n); 
+    
+    for (i=1;i<=n;i++) 
+    { 
+        if(i==1)
+        {
+            s=s+1;
+        }
+        else
+        {
+            s=s+(float)(2*i-1)/(2*i);
+        }
+    } 
+    
+    printf("sum of series: %.1f",s); 
+    return 0; 
+}
