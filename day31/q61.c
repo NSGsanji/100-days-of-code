6e2cb3ab3b30d810e4afb43965a7cf8a8b5cdb8b
Smokister <hardikjoshi246@gmail.com>
2026-09-17 10:27:53 +0530

    Add linear search implementation in q61.c
    Implement linear search algorithm to find an element in an array.

---
+++ day31/q61.c
@@ -0,0 +1,49 @@
+//Q61: Search for an element in an array using linear search.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+3
+Output 1:
+Found at index 2
+
+Input 2:
+4
+10 20 30 40
+25
+Output 2:
+-1
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], search, index = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &search);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == search) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1)
+        printf("Found at index %d", index);
+    else
+        printf("-1");
+
+    return 0;
+}
